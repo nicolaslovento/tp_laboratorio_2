c2709cf800169dd2545641049cb22934338a220e
nicolaslovento <31898579+nicolaslovento@users.noreply.github.com>
2018-09-17 18:40:14 -0300

    Add files via upload

---
+++ TP Nº1/Entidades/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            string retorno = "+";
+            if(operador=="-" || operador=="/" || operador == "*")
+            {
+                retorno = operador;
+            }
+            return retorno; 
+        }
+
+        public double Operar(Numero num1,Numero num2,string operador)
+        {
+            double resultado;
+            string operadorValidado = ValidarOperador(operador);
+
+            if(operadorValidado=="/")
+            {
+                resultado = num1/ num2;
+            }
+            else
+            { 
+                if (operadorValidado == "-")
+                {
+                resultado = num1 - num2;
+                }
+                else { 
+                if(operadorValidado == "*")
+                {
+                    resultado=num1* num2;
+                }
+                else
+                {
+                    resultado = num1 + num2;
+                }
+
+                }
+
+            }
+            
+
+            return resultado;
+        }
+    }
+}
---
+++ TP Nº1/TpCalculadora/LaCalculadora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace TpCalculadora
+{
+    public partial class LaCalculadora : Form
+    {
+        private Calculadora _miCalculadora;
+        private Numero _num1;
+        private Numero _num2;
+
+        public LaCalculadora()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.comboBox.Items.Add("/");
+            this.comboBox.Items.Add("*");
+            this.comboBox.Items.Add("+");
+            this.comboBox.Items.Add("-");
+
+
+
+
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            this._miCalculadora = new Calculadora();
+            this._num1 = new Numero();
+            this._num2 = new Numero();
+
+
+
+
+            _num1.SetNumero = this.textBox1.Text;
+            _num2.SetNumero = this.textBox2.Text;
+            string operador = this.comboBox.Text;
+            if (operador.Length==0)
+            {
+                
+                this.label1.Text ="Seleccione operador.";
+            }
+            else
+            {
+                double resultado = this._miCalculadora.Operar(this._num1, this._num2, operador);
+                this.label1.Text = resultado.ToString();
+            }
+            
+        }
+
+
+
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = Numero.DecimalBinario(this.label1.Text);
+
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = Numero.BinarioDecimal(this.label1.Text);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox || item is Label)
+                {
+                    item.Text = "";
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+
