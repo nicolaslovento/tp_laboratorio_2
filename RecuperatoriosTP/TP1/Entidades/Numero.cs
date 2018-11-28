a1d0e5da7c6b196255248bd0f699a5614b4cfe93
nicolaslovento <31898579+nicolaslovento@users.noreply.github.com>
2018-11-28 15:38:40 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP1/Entidades/Numero.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double _numero;
+
+
+        #region CONSTRUCTORES
+        public Numero()
+        {
+            this._numero = 0;
+        }
+
+        public Numero(double num)
+        {
+            this._numero = num;
+        }
+
+        public Numero(string num)
+        {
+            this._numero = double.Parse(num);
+        }
+
+        #endregion
+
+
+        private double validarNumero(string numIngresado)
+        {
+            bool validacion;
+            double numero;
+            if (numIngresado!="")
+            {
+            numero=double.Parse(numIngresado);
+            validacion = double.IsNaN(numero);
+
+            if(validacion!=true)
+            {
+                return numero;
+            }
+            }
+            return 0;
+        }
+
+        public string SetNumero {
+            set{
+                this._numero = validarNumero(value);
+             } }
+
+        public static string BinarioDecimal(string binario)
+        {
+            double sumador = 0;
+            string retorno = "Valor invalido";
+            if (binario.Length>0)
+            { 
+                for (int x = binario.Length - 1, y = 0; x >= 0; x--, y++)
+                {
+                     if (binario[x] == '1')
+                        {
+                         sumador += 1 * Math.Pow(2, y);
+                        }
+                }
+
+            retorno = sumador.ToString();
+            }
+
+            return retorno;
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string numBinario ="Valor invalido";
+            string residuo;
+            int numero2;
+            if (numero != 0)
+            {
+                numero2 = (int)numero;
+
+                if (numero2 > -1)
+            {
+                numBinario = "";
+                while (numero2 > 0)
+                 {
+                    residuo = (numero2 % 2).ToString();
+                    numBinario = residuo + numBinario;
+                    numero2 = numero2 / 2;
+                }
+            }
+            }
+            return numBinario;
+        }
+
+        public static string DecimalBinario(string numero)
+        {
+            string numBinario="";
+            string residuo;
+            int num;
+            if (numero!="")
+            {
+                numBinario = "Valor invalido";
+                num = int.Parse(numero);
+            
+            if (num > -1)
+            {
+                numBinario = "";
+                while (num > 0)
+                {
+                    residuo = (num % 2).ToString();
+                    numBinario = residuo + numBinario;
+                    num = num / 2;
+                }
+            }
+            }
+            return numBinario;
+        }
+
+        #region operadores
+        public static double operator +(Numero num1,Numero num2)
+        {
+            return num1._numero + num2._numero;
+        }
+
+        public static double operator -(Numero num1, Numero num2)
+        {
+            return num1._numero - num2._numero;
+        }
+        public static double operator /(Numero num1, Numero num2)
+        {
+            return num1._numero / num2._numero;
+        }
+
+        public static double operator *(Numero num1, Numero num2)
+        {
+            return num1._numero * num2._numero;
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ RecuperatoriosTP/TP1/TpCalculadora/LaCalculadora.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TpCalculadora
+{
+    partial class LaCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(29, 177);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(126, 45);
+            this.btnOperar.TabIndex = 0;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(191, 177);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(122, 45);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(351, 177);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(111, 45);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cerrar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.Location = new System.Drawing.Point(89, 240);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(119, 38);
+            this.btnConvertirABinario.TabIndex = 3;
+            this.btnConvertirABinario.Text = "Convertir a Binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(287, 240);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(128, 38);
+            this.btnConvertirADecimal.TabIndex = 4;
+            this.btnConvertirADecimal.Text = "Convertir a Decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(191, 74);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(97, 21);
+            this.comboBox.Sorted = true;
+            this.comboBox.TabIndex = 10;
+            this.comboBox.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 30);
+            this.label1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 39);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 42);
+            this.textBox2.TabIndex = 13;
+            // 
+            // LaCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(487, 290);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LaCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Nicolas Lo Vento 2ºA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
